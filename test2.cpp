b2b5dcfc8053a39f7265538265f46d249e887733
yimingwang800 <yimingwang800@gmail.com>
2021-05-20 19:49:22 -0400

    addtest2.cpp

---
+++ test2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << INT_MAX << endl;
+    cout << UINT_MAX << endl;
+    cout << LLONG_MAX << endl;
+    cout << ULLONG_MAX << endl;
+
+    return 0;
+}
