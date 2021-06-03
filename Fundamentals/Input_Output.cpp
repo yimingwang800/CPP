e41681a86839a7de05d3746e209f97799505c60a
yimingwang800 <yimingwang800@gmail.com>
2021-06-03 19:24:25 -0400

    update

---
+++ Fundamentals/Input_Output.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char x;
+    
+    cin >> x;
+    cout << x << " " << char(65) << " " << x << endl;
+ 
+    return 0;
+}
---
+++ Fundamentals/enter_endl.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    cout << line; // Get the whole line with space from terminal
+ 
+    return 0;
+}
