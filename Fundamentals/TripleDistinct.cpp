060bd9f96b1842b85dcc08887aa37d9bbc923544
yimingwang800 <yimingwang800@gmail.com>
2021-09-16 18:51:50 -0400

    updated

---
+++ Fundamentals/TripleDistinct.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, sum;
+    cout << "Input a number between 6 and 100" << endl;
+    cin >> sum;
+    
+    for (int a = 1; a <= 97; a++) {
+        for (int b = a+1; b <= 97; b++) {
+            for (int c = b+1; c <=97; c++) {
+                if ( a+b+c==sum) {
+                    cout << a << " " << b << " " << c << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
