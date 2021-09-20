0f98ec0bf92356f2c28a8a711f64006f43e5ac25
yimingwang800 <yimingwang800@gmail.com>
2021-09-20 19:27:57 -0400

    updated

--- CCC/2019/CCC '19 J2 - Time to Decompress.cpp
+++ CCC/2019/CCC '19 J2 - Time to Decompress.cpp
@@ -5,7 +5,10 @@ int main() {
     int num_of_lines;
     cin >> num_of_lines;
     for (int l=1; l<=num_of_lines; l++) {
-        cout << "test" << endl;
+        int V1, V2;
+        char V3;
+        cin >> V1 >> V2 >> V3;
+        cout << V1 << V2 << V3 << endl;
 
     }
     
