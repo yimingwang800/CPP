04746e4397acc1097329199d70d8366dfd3e40d8
yimingwang800 <yimingwang800@gmail.com>
2021-10-03 12:04:03 -0400

    updated

--- CCC/2020/CCC '20 J2 - Epidemiology.cpp
+++ CCC/2020/CCC '20 J2 - Epidemiology.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main() {
     int num_people, num_disease_day_0, inffection_rate;
     cin >> num_people >> num_disease_day_0 >> inffection_rate; 
+    int num_inffection_next_day = num_disease_day_0;
     int sum = num_disease_day_0;
-    int num_disease_nextday = num_disease_day_0;
     int total_days = 0;
-    while (sum <= num_people ) {
-        num_disease_nextday = num_disease_nextday * inffection_rate;
-        sum = sum + num_disease_nextday; 
+    
+    while (sum<=num_people) {
+        num_inffection_next_day = num_inffection_next_day*inffection_rate;
+        sum = sum + num_inffection_next_day;
         total_days++;
-        }
-    cout << total_days++ << endl;
-           
+    }
+    cout << total_days << endl;
+    
     return 0;
-
 }
