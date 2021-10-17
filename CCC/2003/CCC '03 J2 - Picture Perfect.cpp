65c9b5f3438f13894cc15b12d2ae730811b03e57
yimingwang800 <yimingwang800@gmail.com>
2021-10-17 10:21:15 -0400

    updated

--- CCC/2003/CCC '03 J2 - Picture Perfect.cpp
+++ CCC/2003/CCC '03 J2 - Picture Perfect.cpp
@@ -7,6 +7,10 @@ int main() {
     while ( area != 0) {
         width = sqrt(area);
         length = area/width;
+        while (length*width != area){
+            width = width-1;
+            length = area/width;
+        }
         perimeter = (width+length)*2;
         cout << "Minimum perimeter is " << perimeter << " with dimensions " << width << " x " << length << endl;
         cin >> area;
