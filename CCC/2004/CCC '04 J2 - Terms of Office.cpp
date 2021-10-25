4c0335b373131aed019341318dda1d9223022c75
yimingwang800 <yimingwang800@gmail.com>
2021-10-24 20:16:04 -0400

    updated

--- CCC/2004/CCC '04 J2 - Terms of Office.cpp
+++ CCC/2004/CCC '04 J2 - Terms of Office.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int this_year, future_year, position_change_year;
 
 void position_switch() {
-    position_change_year = this_year + 60;
+    position_change_year = position_change_year + 60;
 }
 
 int main() {
     cin >> this_year >> future_year;
-    //position_change_year = this_year;
-    cout << "All positions change in year " << endl;
-    while (position_change_year <= future_year) {
+    position_change_year = this_year;
+    cout << "All positions change in year " << position_change_year << endl;
+    while (position_change_year <= future_year - 60) {
         position_switch();
+        cout << "All positions change in year " << position_change_year << endl;
     }
     return 0;
 }
