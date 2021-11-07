5d957480c105e6ab15662f31920d2f73fd2eaef8
yimingwang800 <yimingwang800@gmail.com>
2021-11-07 16:57:49 -0500

    updated

--- CCC/2017/Shifty Sum - Array.cpp
+++ CCC/2017/Shifty Sum - Array.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int num, sum, shifts, shifty_sum, original_num, shifty_sum_next;
 
-    
+int shtifty_sum_function(){
+    original_num = num;
+    int shifty_sum[shifts] = {};
+    for (int i = 1; i <= shifts; i++ ) {
+        num = num*10;
+        shifty_sum[i] = num+shifty_sum[i-1];
+        shifty_sum_next = shifty_sum[i];
+    }
+    sum = original_num+shifty_sum_next;
+    return sum;     // Integer, which is match with the data type of the function
+}
+
+int main() {
+    cin >> num >> shifts;
+    cout << shtifty_sum_function() << endl;
     return 0;
 }
