49f67f87621f9aba0b2b9e5bc3b6d1c452a12577
yimingwang800 <yimingwang800@gmail.com>
2022-02-17 19:12:23 -0500

    updated

--- CCC/2012/CCC '12 J3 - Icon Scaling.cpp
+++ CCC/2012/CCC '12 J3 - Icon Scaling.cpp
@@ -21,26 +21,45 @@ int main() {
 
     for (int i = 0; i < 1*k; i++ ) {
         for (int j = 0; j < 1*k; j++ ) {
-            new_icon_scalling[i][j] = '*';
+            new_icon_scalling[i][j] = icon_scalling [0] [0];
         } 
-    }
-    for (int i = 1*k; i < 2*k; i++ ) {
         for (int j = 1*k; j < 2*k; j++ ) {
-            new_icon_scalling[i][j] = 'x';
+            new_icon_scalling[i][j] = icon_scalling [0] [1];
         } 
-    }
-
-    for (int i = 2*k; i < 3*k; i++ ) {
         for (int j = 2*k; j < 3*k; j++ ) {
-            new_icon_scalling[i][j] = '*';
-        } 
+            new_icon_scalling[i][j] = icon_scalling [0] [2];
+        }                 
     }
 
-    for (int i = 0; i < 6; i++ ) {
-        for (int j = 0; j < 6; j++ ) {
-            cout << new_icon_scalling[i][j]; 
+     for (int i = 1*k; i < 2*k; i++ ) {
+        for (int j = 0; j < 1*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [1] [0];
         } 
-    }cout << endl;
+        for (int j = 1*k; j < 2*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [1] [1];
+        } 
+        for (int j = 2*k; j < 3*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [1] [2];
+        }                 
+    }   
+
+     for (int i = 2*k; i < 3*k; i++ ) {
+        for (int j = 0; j < 1*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [2] [0];
+        } 
+        for (int j = 1*k; j < 2*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [2] [1];
+        } 
+        for (int j = 2*k; j < 3*k; j++ ) {
+            new_icon_scalling[i][j] = icon_scalling [2] [2];
+        }                 
+    }
+
+    for (int i = 0; i < 3*k; i++ ) {
+        for (int j = 0; j < 3*k; j++ ) {
+            cout << new_icon_scalling[i][j]; 
+        } cout << endl;
+    }
 
     return 0;
 }
---
+++ CCC/2016/CCC '16 J2 - Magic Squares-1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sum;
+    int sum_horizontal[4] = {0};
+    int sum_vertical[4] = {0};
+    int square_numbers[4][4];
+    
+    for (int i = 0; i < 4; i++ ) {
+        for (int j = 0; j < 4; j++ ) {
+            cin >> square_numbers[i][j];
+        } 
+    }
+
+    for (int i = 0; i < 4; i++ ){
+        for (int j = 0; j < 4; j++ ){
+            sum_horizontal[i] = sum_horizontal[i] + square_numbers[i][j];
+        }
+    }
+
+    for (int j = 0; j < 4; j++ ){
+        for (int i = 0; i < 4; i++ ){
+            sum_vertical[j] = sum_vertical[j] + square_numbers[i][j];
+        }
+    }
+
+    sum = sum_horizontal[0];
+    for (int i = 0; i < 4; i++) {
+        if (sum_horizontal[i] != sum || sum_vertical[i] != sum) {  
+            cout << "not magic" << endl;
+            break;
+        } else {
+            cout << "magic" << endl;
+            break;
+        }
+    }
+    
+
+    return 0;
+}
