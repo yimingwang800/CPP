d0903e30e109812e4fe96a615df6b78ce727a730
yimingwang800 <yimingwang800@gmail.com>
2022-04-07 18:53:40 -0400

    updated

--- CCC/2018/Bitset - Occupy parking.cpp
+++ CCC/2018/Bitset - Occupy parking.cpp
@@ -12,10 +12,10 @@ int main() {
     for (int i = 0; i < num_parking_space; i++) {
         if (parking_yesterday[i] == 'C' && parking_yesterday[i] == parking_today[i]) {
             both_occupied.set (i, 1);
-            num_occupied_both_days = both_occupied.count();
         }
     }
 
+    num_occupied_both_days = both_occupied.count();
     cout << num_occupied_both_days << endl;
     
     return 0;
