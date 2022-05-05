68aa60ad7ce9f29b0fdf83eff121b415db429ea0
yimingwang800 <yimingwang800@gmail.com>
2022-05-05 19:33:42 -0400

    updated

---
+++ CCC/2010/CCC 2010 Junior - J3 Punchy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int operation, n;
+    map <char, int> data = {
+        {'A', 0},
+        {'B', 0}
+    };
+    char x, y;
+
+    cin >> operation; 
+    while (operation != 7) {
+        
+    }
+    
+    return 0;
+}
