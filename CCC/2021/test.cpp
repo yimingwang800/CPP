e0414d410ef2fe2710e7305b367d04a4f5dfb3c9
yimingwang800 <yimingwang800@gmail.com>
2022-05-05 15:29:47 -0400

    updated

--- CCC/2021/test.cpp
+++ CCC/2021/test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int number_of_bidders; 
     cin >> number_of_bidders;
+    //map <int, string> sorted_Auction_list; 
     map <int, string, greater<int>> Auction_list; 
     string bidder_name;
     int bid_Amount;
@@ -13,7 +14,12 @@ int main() {
        Auction_list.insert(pair<int, string>(bid_Amount,bidder_name));
     }
 
+    for (auto &item : Auction_list) {
+       cout << item.first << " " << item.second << endl;   // ietm.first= key, item.second = element
+    }
+
     cout << Auction_list.begin() -> second << endl;  
+
  
     return 0;
 }
