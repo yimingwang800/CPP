9b063067633b22a8872d1566a03f013625f5fab3
yimingwang800 <yimingwang800@gmail.com>
2022-05-11 20:46:47 -0400

    updated

--- CCC/2010/CCC 2010 Junior - J3 Punchy.cpp
+++ CCC/2010/CCC 2010 Junior - J3 Punchy.cpp
@@ -11,8 +11,49 @@ int main() {
 
     cin >> operation; 
     while (operation != 7) {
-        
+        if (operation == 1) {
+            cin >> x >> n;
+            if (x == 'A'){
+                data['A'] = n;
+            }
+            if (x == 'B'){
+                data['B'] = n;
+            }
+        }
+
+        if (operation == 2) {
+            cin >> x;
+            if (x == 'A'){
+                cout << data['A'] << endl;
+            }
+            if (x == 'B'){
+                cout << data['B'] << endl;
+            }
+        }
+
+        if (operation == 3) {
+            cin >> x >> y;
+            data[x] = data[x] + data[y];
+        }
+
+        if (operation == 4) {
+            cin >> x >> y;
+            data[x] = data[x] * data[y];
+        }
+
+        if (operation == 5) {
+            cin >> x >> y;
+            data[x] = data[x] - data[y];
+        }
+
+        if (operation == 6) {
+            cin >> x >> y;
+            data[x] = data[x] / data[y];
+        }
+
+        cin >> operation; 
     }
+    
     
     return 0;
 }
